a534b75bee61f908a78d4307f26b4a895023ff40
nathan-zucker <114187880+nathan-zucker@users.noreply.github.com>
2023-01-05 01:02:32 -0500

    fix

--- src/Components/DisplayBaseStats.js
+++ src/Components/DisplayBaseStats.js
@@ -3,16 +3,16 @@ import { useSelector } from "react-redux";
 
 export const DisplayBaseStats = () => {
     const stats = useSelector((state)=>state.baseStats.stats)
-    //const bonus = useSelector((state)=>state.raceDetails.abilityScoreIncrease)
-
+    const bonus = useSelector((state)=>state.raceDetails.abilityScoreIncrease)
+    
     
     const display = ([
-        ['Strength: ', stats[0] || null], 
-        ['Dexterity: ', stats[1] || null], 
-        ['Constitution: ', stats[2] || null], 
-        ['Intelligence: ', stats[3] || null], 
-        ['Wisdom: ', stats[4] || null], 
-        ['Charisma: ', stats[5] || null]
+        ['Strength: ', stats[0] || null, "+",bonus[0]], 
+        ['Dexterity: ', stats[1] || null, "+",bonus[1]], 
+        ['Constitution: ', stats[2] || null, "+",bonus[2]], 
+        ['Intelligence: ', stats[3] || null, "+",bonus[3]], 
+        ['Wisdom: ', stats[4] || null, "+",bonus[4]], 
+        ['Charisma: ', stats[5] || null, "+",bonus[5]]
     ]).map((i)=><div key={i} className='statDisplay'><h3>{i.join('')}</h3></div>)
     
     return(
